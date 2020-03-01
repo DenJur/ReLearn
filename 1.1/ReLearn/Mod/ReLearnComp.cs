422a4d41e0a813008f7c30a39996c19cc674b61e
DenJur <deniss.jurockins02@gmail.com>
2020-03-01 13:35:40 +0000

    Update for game version 1.1

---
+++ 1.1/ReLearn/Mod/ReLearnComp.cs
@@ -0,0 +1,37 @@
+﻿using Verse;
+
+namespace ReLearn
+{
+    public class ReLearnComp : ThingComp
+    {
+        private ExperienceTracker _experience;
+
+        public ExperienceTracker Experience
+        {
+            get
+            {
+                if (_experience == null)
+                {
+                    if (parent is Pawn pawn)
+                    {
+                        _experience = new ExperienceTracker(pawn);
+                        _experience.FillExperience();
+                    }
+                    else
+                    {
+                        Log.Error("ReLearn: Tried to add Comp to " + parent.Label + " but it is not a pawn.");
+                    }
+                }
+
+                return _experience;
+            }
+            set => _experience = value;
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Deep.Look(ref _experience, "experience", parent as Pawn);
+        }
+    }
+}
